ac6d1560b470eaa46937dbf4979356fec52fd6c7
cloudylogs <alison.liu999@gmail.com>
2023-01-18 21:46:27 -0800

    finished four basic functions
    finished four basic functions to do simple arithmetic, have not thought about constraints yet. working on % and inverting

--- script.js
+++ script.js
@@ -10,16 +10,75 @@ class Calculator {
         this.operation = undefined;
     }
     appendNumber(number) {
-        this.currentOperand = number;
+        if (this.currentOperand.includes(".") && number === ".") return;
+        this.currentOperand =
+            this.currentOperand.toString() + number.toString();
     }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === "") return;
+        if (this.previousOperand !== "") {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
+
+    compute() {
+        let solution;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case "+":
+                solution = prev + current;
+                break;
+            case "-":
+                solution = prev - current;
+                break;
+            case "*":
+                solution = prev * current;
+                break;
+            case "÷":
+                solution = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = solution;
+        this.operation = undefined;
+        this.previousOperand = "";
+    }
+    percent() {
+        let percentage;
+        switch (this.operation) {
+            case "%":
+                percentage = parseFloat(this.currentOperand / 100)
+                break
+        }
+        this.currentOperand = percentage;
+        this.operation = undefined;
+        this.previousOperand = "";
+        console.log(percentage);
+    }
+
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
-        console.log(this.currentOperand)
+        if (this.operation !== undefined) {
+            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = "";
+        }
     }
 }
 
 const clearButton = document.querySelector("[data-clear]");
 const numberButtons = document.querySelectorAll("[data-number]");
+const operationButton = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const percentButton = document.querySelector("[data-percent]");
+const invertButton = document.querySelector("[data-invert]");
 const previousOperandTextElement = document.querySelector(
     "[data-previous-operand]"
 );
@@ -28,13 +87,40 @@ const currentOperandTextElement = document.querySelector(
 );
 
 const calculator = new Calculator(
-    previousOperandTextElement, 
+    previousOperandTextElement,
     currentOperandTextElement
 );
 
-numberButtons.forEach(button => {
+numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
-    })
-})
+    });
+});
+
+clearButton.addEventListener("click", () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+operationButton.forEach((button) => {
+    button.addEventListener("click", () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener("click", () => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+percentButton.addEventListener("click", () => {
+    calculator.percent();
+    calculator.updateDisplay();
+});
+
+invertButton.addEventListener("click", () => {
+    calculator.invert();
+    calculator.updateDisplay();
+});
